67c2d8d30d35a9637a39684357404b0ec8b689e6
Ashung (iMac) <ashung.hung@gmail.com>
2016-09-09 19:43:34 +0800

    add android shell

--- automate-sketch.sketchplugin/Contents/Sketch/Shell/Android_Asset_Folder.sh
+++ automate-sketch.sketchplugin/Contents/Sketch/Shell/Android_Asset_Folder.sh
@@ -1,5 +1,49 @@
+#!/usr/bin/env bash
 
-# cd ~/Desktop
+# Replace space to "_"
+for img in *.png; do
+    name=$(echo ${img} | sed 's/[[:space:]]/_/g')
+    mv "${img}" ${name}
+done
 
-mkdir drawable-mdpi drawable-hdpi drawable-xhdpi drawable-xxhdpi drawable-xxxhdpi
- # for img in $(ls | grep *_hdpi.png); do mv $img ./drawable-hdpi/$img; done
+# Make dir
+if [ ! -d drawable-mdpi ]; then
+    mkdir drawable-mdpi
+fi
+
+if [ ! -d drawable-hdpi ]; then
+    mkdir drawable-hdpi
+fi
+
+if [ ! -d drawable-xhdpi ]; then
+    mkdir drawable-xhdpi
+fi
+
+if [ ! -d drawable-xxhdpi ]; then
+    mkdir drawable-xxhdpi
+fi
+
+if [ ! -d drawable-xxxhdpi ]; then
+    mkdir drawable-xxxhdpi
+fi
+
+# Copy _?dpi.png to drawable-?dpi folder
+for img in $(ls | grep _mdpi.png); do
+    mv ${img} drawable-mdpi/$(echo ${img} | sed 's/_mdpi//')
+done
+
+for img in $(ls | grep _hdpi.png); do
+    mv ${img} drawable-hdpi/$(echo ${img} | sed 's/_hdpi//')
+done
+
+for img in $(ls | grep _xhdpi.png); do
+    mv ${img} drawable-xhdpi/$(echo ${img} | sed 's/_xhdpi//')
+done
+
+for img in $(ls | grep _xxhdpi.png); do
+    mv ${img} drawable-xxhdpi/$(echo ${img} | sed 's/_xxhdpi//')
+done
+
+for img in $(ls | grep _xxxhdpi.png); do
+    mv ${img} drawable-xxxhdpi/$(echo ${img} | sed 's/_xxxhdpi//')
+done
