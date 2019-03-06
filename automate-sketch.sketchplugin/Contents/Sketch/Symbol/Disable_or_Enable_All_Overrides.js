7c2e189da33beaa9fbd8e8beaf0533fd722b7b7d
Ashung Hung <Ashung.hung@gmail.com>
2019-03-07 00:21:35 +0800

    - Add Copy SVG Path Data. - Add Insert Layer from SVG Path Data. - Add Insert Layers from SVG Code. - Add Disable or Enable All Overrides.

--- automate-sketch.sketchplugin/Contents/Sketch/Symbol/Disable_or_Enable_All_Overrides.js
+++ automate-sketch.sketchplugin/Contents/Sketch/Symbol/Disable_or_Enable_All_Overrides.js
@@ -4,23 +4,29 @@ var onRun = function(context) {
 
     ga(context, "Symbol");
 
+    var util = require("util");
     var sketch = require("sketch");
-    var pluginIdentifier = context.command.identifier();
     var document = sketch.getSelectedDocument();
     var selection = document.selectedLayers.layers;
 
     var symbolID = [];
     var selectedSymbol = selection.filter(function(layer) {
-        if (layer.type == "SymbolMaster" || layer.type == "SymbolInstance") {
+        // Only local symbol instance and symbol master
+        if (
+            layer.type == "SymbolMaster" ||
+            (layer.type == "SymbolInstance" && !layer.master.sketchObject.isForeign())
+        ) {
             return layer;
         }
     }).map(function(layer) {
+        // Map to symbol master
         if (layer.type == "SymbolInstance") {
             return layer.master;
         } else {
             return layer;
         }
     }).filter(function(layer) {
+        // Remove the duplicated
         if (!symbolID.includes(layer.id)) {
             symbolID.push(layer.id);
             return layer;
@@ -28,26 +34,40 @@ var onRun = function(context) {
     });
 
     if (selectedSymbol.length == 0) {
-        sketch.UI.message("Please select at least 1 symbol instance or master.");
+        sketch.UI.message("Please select at least 1 local symbol instance or master.");
         return;
     }
 
+    var isAllOverridesDisable;
+
     selectedSymbol.forEach(function(symbol) {
+
+        var overridePropertyKeys = symbol.sketchObject.overrideProperies().allKeys();
+        var idsOfOverrideInManagePanel = util.toArray(overridePropertyKeys).map(function(item) {
+            return String(item);
+        });
         
-        var overridePoints = symbol.overrides.map(function(override) {
+        var overridesInManagePanel = symbol.overrides.filter(function(override) {
+            return idsOfOverrideInManagePanel.includes(override.id);
+        });
+
+        var overridePoints = overridesInManagePanel.map(function(override) {
             return override.sketchObject.overridePoint();
         });
 
+        isAllOverridesDisable = overridesInManagePanel.every(function(override) {
+            return override.editable == false;
+        });
+
         overridePoints.forEach(function(override) {
-            var idToBoolean = (pluginIdentifier == "enable_all_overrides") ? true : false;
-            symbol.sketchObject.setOverridePoint_editable(override, idToBoolean);
+            symbol.sketchObject.setOverridePoint_editable(override, isAllOverridesDisable);
         });
 
     });
 
     document.sketchObject.reloadInspector();
 
-    var message = ((pluginIdentifier == "enable_all_overrides") ? "Enable " : "Disable ") +
+    var message = ((isAllOverridesDisable) ? "Enable " : "Disable ") + "all overrides for " +
         selectedSymbol.length + " symbol master" +
         ((selectedSymbol.length > 1) ? "s." : ".");
 
