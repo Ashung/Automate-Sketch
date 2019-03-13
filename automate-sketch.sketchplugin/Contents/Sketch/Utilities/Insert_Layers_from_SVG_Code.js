381eb9d721babadd0d0b1265285765b2d10941ef
Ashung <ashung.hung@gmail.com>
2019-03-13 19:46:39 +0800

    - Improve Insert Layer from SVG Path Data.

--- automate-sketch.sketchplugin/Contents/Sketch/Utilities/Insert_Layers_from_SVG_Code.js
+++ automate-sketch.sketchplugin/Contents/Sketch/Utilities/Insert_Layers_from_SVG_Code.js
@@ -7,18 +7,35 @@ var onRun = function(context) {
 
     var sketch = require("sketch");
     var document = sketch.getSelectedDocument();
+    var identifier = context.command.identifier();
 
     var dialog = UI.cosDialog(
         "Insert Layers from SVG Code"
     );
-
     var input = UI.textField("", [300, 100]);
+
+    if (identifier == "insert_layer_from_svg_path_data") {
+        dialog = UI.cosDialog(
+            "Insert Layer from SVG Path Data"
+        );
+        input = UI.textField("", [300, 50]);
+    }
+
     dialog.addAccessoryView(input);
 
     var responseCode = dialog.runModal();
     if (responseCode == 1000) {
 
+        if (!input.stringValue()) {
+            return;
+        }
+
         var svgString = input.stringValue();
+
+        if (identifier == "insert_layer_from_svg_path_data") {
+            svgString = NSString.stringWithFormat('<svg><path d="%@"></path></svg>', svgString);
+        }
+
         var svgData = svgString.dataUsingEncoding(NSUTF8StringEncoding);
         var svgImporter = MSSVGImporter.svgImporter();
         svgImporter.prepareToImportFromData(svgData);
