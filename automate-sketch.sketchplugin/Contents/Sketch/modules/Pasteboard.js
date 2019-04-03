fec2104e039f2f545159eb9f13b8aecc1e8df42e
Ashung <ashung.hung@gmail.com>
2019-04-03 18:32:19 +0800

    Add Paste as Artboards / Symbol Masters.

--- automate-sketch.sketchplugin/Contents/Sketch/modules/Pasteboard.js
+++ automate-sketch.sketchplugin/Contents/Sketch/modules/Pasteboard.js
@@ -1,6 +1,18 @@
 
+var pasteboard = NSPasteboard.generalPasteboard();
+
 module.exports.pbcopy = function(text) {
-    var pasteboard = NSPasteboard.generalPasteboard();
     pasteboard.clearContents();
-    pasteboard.setString_forType(text, NSStringPboardType);
+    pasteboard.setString_forType(text, NSPasteboardTypeString);
+};
+
+module.exports.layersFromPasteboard = function(context){
+    var pasteboardManager = AppController.sharedInstance().pasteboardManager();
+    var pasteboardLayers = pasteboardManager.readPasteboardLayersFromPasteboard_colorSpace_options(
+        pasteboard, context.document.colorSpace(), nil
+    );
+    if (pasteboardLayers == null) {
+        return;
+    }
+    return pasteboardLayers.layers().layers();
 };
