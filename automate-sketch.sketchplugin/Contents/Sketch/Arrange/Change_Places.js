4a1d243d87bdf870d54203ad1bf02a07726e3efa
Ashung <ashung.hung@gmail.com>
2019-06-26 20:09:42 +0800

    Fix Change Places move layer to top.

--- automate-sketch.sketchplugin/Contents/Sketch/Arrange/Change_Places.js
+++ automate-sketch.sketchplugin/Contents/Sketch/Arrange/Change_Places.js
@@ -14,20 +14,23 @@ var onRun = function(context) {
 
     var layer1Artboard, layer1ParentGroup, layer1BeforeLayer, layer1Frame, 
         layer2Artboard, layer2ParentGroup, layer2BeforeLayer, layer2Frame;
-    if (selection[0].parentArtboard()) {
-        layer1Artboard = selection[0].parentArtboard();
-        layer1ParentGroup = selection[0].parentGroup();
-        layer1Frame = selection[0].frame();
-        layer1BeforeLayer = (layer1ParentGroup.layers().count() == layer1ParentGroup.indexOfLayer(selection[0]) + 1) ? nil : layer1ParentGroup.layers().objectAtIndex(layer1ParentGroup.indexOfLayer(selection[0]) + 1);
-        selection[0].moveToLayer_beforeLayer(selection[0].parentPage(), nil);
-    }
-
-    if (selection[1].parentArtboard()) {
-        layer2Artboard = selection[1].parentArtboard();
-        layer2ParentGroup = selection[1].parentGroup();
-        layer2Frame = selection[1].frame();
-        layer2BeforeLayer = (layer2ParentGroup.layers().count() == layer2ParentGroup.indexOfLayer(selection[1]) + 1) ? nil : layer2ParentGroup.layers().objectAtIndex(layer2ParentGroup.indexOfLayer(selection[1]) + 1);
-        selection[1].moveToLayer_beforeLayer(selection[1].parentPage(), nil);
+    
+    // If layers no in same group
+    if (selection[0].parentGroup() != selection[1].parentGroup()) {
+        if (selection[0].parentArtboard()) {
+            layer1Artboard = selection[0].parentArtboard();
+            layer1ParentGroup = selection[0].parentGroup();
+            layer1Frame = selection[0].frame();
+            layer1BeforeLayer = (layer1ParentGroup.layers().count() == layer1ParentGroup.indexOfLayer(selection[0]) + 1) ? nil : layer1ParentGroup.layers().objectAtIndex(layer1ParentGroup.indexOfLayer(selection[0]) + 1);
+            selection[0].moveToLayer_beforeLayer(selection[0].parentPage(), nil);
+        }
+        if (selection[1].parentArtboard()) {
+            layer2Artboard = selection[1].parentArtboard();
+            layer2ParentGroup = selection[1].parentGroup();
+            layer2Frame = selection[1].frame();
+            layer2BeforeLayer = (layer2ParentGroup.layers().count() == layer2ParentGroup.indexOfLayer(selection[1]) + 1) ? nil : layer2ParentGroup.layers().objectAtIndex(layer2ParentGroup.indexOfLayer(selection[1]) + 1);
+            selection[1].moveToLayer_beforeLayer(selection[1].parentPage(), nil);
+        }
     }
 
     var x1 = selection[0].frame().x(),
