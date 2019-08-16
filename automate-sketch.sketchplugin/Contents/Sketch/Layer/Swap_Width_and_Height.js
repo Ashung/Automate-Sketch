532abf6320f9ddda52266dee65b80ef4685543a8
Ashung <ashung.hung@gmail.com>
2019-08-16 19:40:51 +0800

    - Add Swap Width and Height.

---
+++ automate-sketch.sketchplugin/Contents/Sketch/Layer/Swap_Width_and_Height.js
@@ -0,0 +1,22 @@
+var onRun = function(context) {
+
+    var ga = require("../modules/Google_Analytics");
+    ga("Layer");
+
+    var util = require("util");
+    var sketch = require("sketch");
+    var selectedLayers = sketch.getSelectedDocument().selectedLayers.layers;
+
+    if (selectedLayers.length == 0) {
+        sketch.UI.showMessage("Please select at least 1 layer.")
+        return;
+    }
+
+    selectedLayers.forEach(function(layer) {
+        var width = layer.frame.width;
+        var height = layer.frame.height;
+        layer.frame.width = height;
+        layer.frame.height = width;
+    });
+
+};
