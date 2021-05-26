e298148d6dfc5d5da68d0aaab66374c54aab9cc4
Ashung Hung <ashung.hung@gmail.com>
2021-05-26 20:57:13 +0800

    Fix Auto/Fast Slice bug; Plugin menu language will same as Sketch language.

--- automate-sketch.sketchplugin/Contents/Sketch/Slice/Fast_Slice.js
+++ automate-sketch.sketchplugin/Contents/Sketch/Slice/Fast_Slice.js
@@ -4,7 +4,6 @@ var runFastSlice = function(context) {
     ga("Slice");
 
     var preferences = require("../modules/Preferences");
-    var help = require("../modules/Help");
     var document = context.document;
     var exportPresets = MSExportPreset.allExportPresets();
 
@@ -57,8 +56,6 @@ var runFastSlice = function(context) {
     while (layer = loopSelection.nextObject()) {
 
         var slice = MSSliceLayer.sliceLayerFromLayer(layer);
-        var msRect = help.getMSRectFromMSLayers([slice, layer]);
-        slice.absoluteRect().setRect(msRect.rect());
         slice.makeOriginIntegral();
 
         // layer order
